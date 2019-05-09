7beafcd82e5cf69c0d252b4ae5314ad533bdb963
Chaynor Hsiao <chaynsaw@gmail.com>
2019-05-08 23:29:29 -0700

    Implement basic functionality + manual mysql query on server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,74 @@
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.switchStage = this.switchStage.bind(this);
+    this.state = {
+      name: '',
+      email: 'blagoyevich',
+      password: '',
+      stage: ['f1', 'f2', 'f3'],
+      currentStage: 'f1'
+    }
+  }
+  switchStage() {
+    switch(this.state.currentStage) {
+      case 'f1':
+        this.setState({currentStage: 'f2'});
+        return;
+      case 'f2':
+        this.setState({currentStage: 'f3'});
+        return; 
+      case 'f3':
+        this.setState({currentStage: 'f1'});
+        return; 
+    }
+  }
+  render(props) {
+    return(
+      <form>
+        <NameField currentStage={this.state.currentStage}/>
+        <EmailField currentStage={this.state.currentStage}/>
+        <Password currentStage={this.state.currentStage}/>
+        {/* <InputField things = {this.state}/> */}
+        <NextButton onClick={this.switchStage} />
+      </form>
+    )
+  }
+}
 
+// var InputField = ({bows}) => {
+//   return (<div>{bows}
+//     </div>)
+// }
+var NameField = (props) => {
+  if (props.currentStage === 'f1') {
+    return (
+    <div>Name
+      <input type="text">
+      </input>
+    </div>)
+  } else {
+    return null;
+  }
+}
+  
 
+var EmailField = (props) => (
+  <div>Email
+    <input type="email">
+    </input>
+  </div> 
+)
+var Password = (props) => (
+  <div>Password
+    <input type="password">
+    </input>
+  </div>
+)
+var NextButton = (props) => (
+  <input type="button" value="Next Stage" onClick={props.onClick}>
+  </input>
+)
+
+ReactDOM.render(<Form />, document.getElementById('root'))
+// ReactDOM.render(<StageSelector />, document.getElementById('root'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,36 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var mysql = require('mysql')
 
+var port = 3000;
 
+app.listen(port, console.log(`Listening on port ${port}`));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'chat'
+})
+
+db.connect()
+// function(err) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log('Connected to DB!');
+// })
+
+app.get('/query', (req, res) => {
+  db.query('select * from pet;', (err, results, fields) => {
+    if (err){
+      console.log('fuck')
+      throw err;
+    }
+    res.json(results);
+  })
+})
